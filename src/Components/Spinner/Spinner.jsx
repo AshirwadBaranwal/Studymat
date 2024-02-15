60e3d6b9f8b4b4dd59d3dd69f282b89a7774df37
AshirwadBaranwal <ashirwadkumar77566@gmail.com>
2024-02-15 16:55:59 +0530

    sudhar

--- src/Components/Spinner/Spinner.jsx
+++ src/Components/Spinner/Spinner.jsx
@@ -3,15 +3,15 @@ import "./Spinner.css"
 
 function Spinner() {
   return (
-    <div class="dot-spinner">
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
-      <div class="dot-spinner__dot"></div>
+    <div className="dot-spinner">
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
+      <div className="dot-spinner__dot"></div>
     </div>
   );
 }
