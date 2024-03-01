d043f7d1701dfa091406d4365271f517895d0e1b
AshirwadBaranwal <ashirwadkumar77566@gmail.com>
2024-03-02 01:06:18 +0530

    mistake resolve

--- src/Pages/syllabus/Syllabus.jsx
+++ src/Pages/syllabus/Syllabus.jsx
@@ -4,37 +4,37 @@ import style from "./Syllabus.module.css";
 function Syllabus() {
   return (
     <section className={style.mainpage}>
-      <h6>Syllabus</h6>
+      <h6 className={style.heading}>Syllabus</h6>
       <p className={style.guidline}>
         As per guidelines of the UGC/Patliputra University (Hons.) Paper
       </p>
-      <h6>Hons. — First Year</h6>
-      <h6>PAPER-I</h6>
-      <h6>Group- 1</h6>
+      <h6 className={style.heading}>Hons. — First Year</h6>
+      <h6 className={style.heading}>PAPER-I</h6>
+      <h6 className={style.heading}>Group- 1</h6>
 
       {/* ********COMPUTER FUNDAMENTALS*********      */}
       {/* ********COMPUTER FUNDAMENTALS*********      */}
 
-      <h6>COMPUTER FUNDAMENTALS</h6>
+      <h6 className={style.heading}>COMPUTER FUNDAMENTALS</h6>
       <div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>
             Introduction to Computers :{" "}
           </span>
           What is Computer? Comparison between computer and human brain,
           Characteristics of computer, Computer applications.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>History of Computers : </span>
           Initial development, Generation of computer, Evolution of Personal
           Computer.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Computer Organization : </span>
           Basic Units of computer, Block diagram of computer, Input unit,
           Processing unit, Output unit, Storage unit.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Types of Printers : </span>Hard
           copy output, Impact printers, Non-impact printers, Serial and line
           printers, Dot-Matrix printers, Laser printers, Daisy wheel printers,
@@ -43,30 +43,30 @@ function Syllabus() {
           Magnetic Disk, Tracks, Sectors, Seek time, Rotational latency, Access
           time, Numerical problems.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Types of Computers : </span>
           Digital, Analog, Hybrid Computers, General purpose Computers, Turnkey
           Systems, Micro computers, Mini computers, Mainframes, Super Computers.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Data Representation : </span>
           Number Systems, Binary system, Binary to Decimal and Decimal to Binary
           conversion. Binary addition, 2's compliment representation, Binary
           subtraction, ASCII and EBCDIC coding.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Computer Software : </span>{" "}
           Machine language, Assembly language, High level languages, Compilers,
           Interpreters, Assemblers. Centralized processing, Decentralized
           processing, Distributed processing, Management Information System.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Processing Modes : </span>
           Uniprocessor, Multiprocessor, Batch processing, offline data entry,
           On-line processing, On-line data entry, real Time processing, Time
           sharing processing, Electronic mail, Tele text, Tele conferencing.
         </div>
-        <div>
+        <div className={style.div}>
           <span className={style.chapterName}>Programming Concepts : </span>
           Programme definition, Characteristics of good programme, Programming
           steps, algorithms, Flowcharts.
